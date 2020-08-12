03548a37ce4fa8f9162d550bb3a2361c97f84be1
SamComber <sam.comber@hotmail.co.uk>
2020-08-12 20:22:00 +0100

    attempted to parallelise fold construction

--- spacv/base_classes.py
+++ spacv/base_classes.py
@@ -4,6 +4,10 @@ import geopandas as gpd
 from sklearn.model_selection import BaseCrossValidator
 from .utils import convert_geoseries, convert_geodataframe
 
+import itertools
+from functools import partial
+import multiprocessing as mp
+
 class BaseSpatialCV(BaseCrossValidator, metaclass=ABCMeta):
     """
     Base class for partitioning-based spatial cross-validation approaches.
@@ -59,32 +63,50 @@ class BaseSpatialCV(BaseCrossValidator, metaclass=ABCMeta):
             test_index = indices[test_indices]      
             yield train_index, test_index
         
-    def _build_fold_indices(self, fold_ids, XYs, scv_type, grid=None):
+        
+    def test_para(self, fold_id, XYs, scv_type, grid):
+        if scv_type == 'hblock' or scv_type == 'user':
+            test_indices = XYs.loc[XYs['grid_id'] == fold_id ].index.values
+            fold_polygon = grid.loc[[fold_id]].buffer(self.buffer_radius)
+            # Remove empty grids
+            if len(test_indices) < 1:
+                return
+        elif scv_type == 'sloo':
+            test_indices = np.array([fold_id])
+            fold_polygon = XYs.loc[test_indices].buffer(self.buffer_radius)
+        elif scv_type == 'lattice':
+            test_indices = np.array(fold_id)
+            fold_polygon = XYs.loc[test_indices].unary_union.buffer(self.buffer_radius) 
+        elif scv_type == 'skcv':
+            test_indices = np.array(fold_id)
+            fold_polygon = XYs.loc[test_indices].unary_union.convex_hull.buffer(self.buffer_radius)
+        else:
+            raise Exception('Invalid scv_type')
+
+        test_indices, train_exclude = \
+            self._remove_buffered_indices(XYs, test_indices, 
+                                                self.buffer_radius, fold_polygon)
+        return test_indices, train_exclude 
+        
+    def _build_fold_indices(self, fold_ids, XYs, scv_type, grid=None, n_jobs=1):
     
-        for fold_id in fold_ids:           
+        if n_jobs > 1:
+            
+            func = partial(self.test_para, XYs=XYs, scv_type=scv_type, grid=grid)
+            
+            a = mp.Pool(processes=n_jobs).map(func, fold_ids)
+            
+        else:
+            print('nt para')
+            a = [self.test_para(fold_id, XYs, scv_type, grid) for fold_id in fold_ids]
+#             a = itertools.chain(*a)
 
-            if scv_type == 'hblock' or scv_type == 'user':
-                test_indices = XYs.loc[XYs['grid_id'] == fold_id ].index.values
-                fold_polygon = grid.loc[[fold_id]].buffer(self.buffer_radius)
-                # Remove empty grids
-                if len(test_indices) < 1:
-                    continue
-            elif scv_type == 'sloo':
-                test_indices = np.array([fold_id])
-                fold_polygon = XYs.loc[test_indices].buffer(self.buffer_radius)
-            elif scv_type == 'lattice':
-                test_indices = np.array(fold_id)
-                fold_polygon = XYs.loc[test_indices].unary_union.buffer(self.buffer_radius) 
-            elif scv_type == 'skcv':
-                test_indices = np.array(fold_id)
-                fold_polygon = XYs.loc[test_indices].unary_union.convex_hull.buffer(self.buffer_radius)
-            else:
-                raise Exception('Invalid scv_type')
+        return a
 
-            test_indices, train_exclude = \
-                self._remove_buffered_indices(XYs, test_indices, 
-                                            self.buffer_radius, fold_polygon)
-            yield test_indices, train_exclude 
+        
+        
+        
+        
         
     
     def _remove_buffered_indices(self, XYs, test_indices, buffer_radius, geometry_buffer):
